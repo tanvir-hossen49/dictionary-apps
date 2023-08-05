dfd7c1d3ec30e4663cb40f462f5024a051c6eab3
tanvir-hossen49 <thossen49@gmail.com>
2023-08-05 15:56:01 +0600

    refectore js code

--- js/main.js
+++ js/main.js
@@ -1,81 +1,78 @@
-window.onload = () => {
-    main()
-}
+const userInputEl = document.getElementById("input-field");
 
-//main function
-function main() {
-    const userInputEl = document.getElementById('input-field');
-
-    userInputEl.addEventListener('keyup', function (e) {
-        if (e.key === 'Enter') {
-            let userValue = e.target.value.toLowerCase();
-            findTargetObject(userValue);
-            e.target.value = '';
-        }
-    });
-}
-
-//find targeted object 
-function findTargetObject(userInput) {
-    for (let i = 0; i < dictionary.length; i++) {
-        if (userInput === dictionary[i].en) {
-            showDOM(dictionary[i]);
-            return;
-        } else {
-            showError(userInput);
-        }
+//find targeted object
+const findTargetObject = userInput => {
+  for (let word of dictionary) {
+    if (userInput === word.en) {
+      showDOM(word);
+      return;
+    } else {
+      showError(userInput);
     }
-}
+  }
+};
+
 // show user search value into dom
-function showDOM(targetOBJ) {
-    console.log(targetOBJ)
-    const meaningEL = document.getElementById('meaning'),
-        senteceEl = document.getElementById('example'),
-        banglaSynonyms = document.getElementById('synonymsBangla'),
-        englishSynonyms = document.getElementById('synonymsEnglish'),
-        errorContainer = document.querySelector('.error');
-    showDetailsEl = document.querySelector('.show-details'),
-        userInputEl = document.getElementById('input-field');
+const showDOM = targetOBJ => {
+  const meaningEL = document.getElementById("meaning"),
+    sentenceEle = document.getElementById("example"),
+    banglaSynonyms = document.getElementById("synonymsBangla"),
+    englishSynonyms = document.getElementById("synonymsEnglish"),
+    errorContainer = document.querySelector(".error"),
+    showDetailsEl = document.querySelector(".show-details"),
+    userInputEl = document.getElementById("input-field");
 
-    errorContainer.style.display = 'none';
-    showDetailsEl.classList.add('active');
+  errorContainer.style.display = "none";
+  showDetailsEl.classList.add("active");
 
-    meaningEL.innerHTML = targetOBJ.bn;
-    senteceEl.innerHTML = '';
+  meaningEL.innerHTML = targetOBJ.bn;
+  sentenceEle.innerHTML = "";
 
-    targetOBJ.sents.forEach(sentence => {
-        senteceEl.innerHTML += `${sentence} <br>`;
-    });
+  targetOBJ.sents.forEach(sentence => {
+    sentenceEle.innerHTML += `${sentence} <br>`;
+  });
 
-    targetOBJ.bn_syns.forEach(syns => {
-        banglaSynonyms.innerHTML += `
+  targetOBJ.bn_syns.forEach(syns => {
+    banglaSynonyms.innerHTML += `
             <p>${syns} <br> </p>
         `;
-    })
+  });
 
-    targetOBJ.en_syns.forEach(syns => {
-        englishSynonyms.innerHTML += `
+  targetOBJ.en_syns.forEach(syns => {
+    englishSynonyms.innerHTML += `
             <p><a href='#'>${syns}</a> <br> </p>
         `;
+  });
+
+  [...englishSynonyms.children].forEach(ele => {
+    ele.addEventListener("click", e => {
+      let userText = ele.textContent.trim();
+      userInputEl.value = userText;
+      findTargetObject(userText);
     });
+  });
+};
 
-    [...englishSynonyms.children].forEach(ele => {
-        ele.addEventListener('click', function (e) {
-            let userText = ele.textContent.trim();
-            userInputEl.value = userText;
-            findTargetObject(userText)
-        })
-    })
-}
 // show error when data no found
-function showError(userInput) {
-    const errorContainer = document.querySelector('.error'),
-        showDetailsEl = document.querySelector('.show-details');
+const showError = userInput => {
+  const errorContainer = document.querySelector(".error"),
+    showDetailsEl = document.querySelector(".show-details");
 
-    showDetailsEl.classList.remove("active")
+  showDetailsEl.classList.remove("active");
 
-    errorContainer.style.display = 'block'
-    errorContainer.innerHTML = `
+  errorContainer.style.display = "block";
+  errorContainer.innerHTML = `
         <span class='error-text'>"${userInput}" not found</span>
     `;
-}
+};
+
+// chen user hit enter or not
+const pressEnter = e => {
+  if (e.key === "Enter") {
+    let userValue = e.target.value.toLowerCase();
+    findTargetObject(userValue);
+    e.target.value = "";
+  }
+};
+
+userInputEl.addEventListener("keyup", pressEnter);
